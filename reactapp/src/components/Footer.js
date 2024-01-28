40589925fb102406016ac810dfde5a9e0ebf5bd6
Virginia Wanjiru <virginiashee55@gmail.com>
2024-01-28 18:49:02 +0300

    refactor:add images to images folder

---
+++ reactapp/src/components/Footer.js
@@ -0,0 +1,12 @@
+// components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      {/* Footer content goes here */}
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ reactapp/src/components/Navigation.js
@@ -0,0 +1,12 @@
+// components/Navigation.js
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav>
+      {/* Navigation links go here */}
+    </nav>
+  );
+}
+
+export default Navigation;
