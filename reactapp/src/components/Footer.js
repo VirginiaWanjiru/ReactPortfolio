79559a05a3c7e9df87c6644b4e133eedb1520483
Virginia Wanjiru <virginiashee55@gmail.com>
2024-01-29 15:30:07 +0300

    refactor: Add emoji next to Hobbies

--- reactapp/src/components/Footer.js
+++ reactapp/src/components/Footer.js
@@ -6,14 +6,16 @@ function ContactInfo() {
     <footer>
     
       <p>Contact me:</p>
-      <div className="footer">
+      <div className="contact-list">
         
         <a href="https://www.linkedin.com/in/virginia-wanjiru-0852641b0/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
           <img src="./images/linkedin.svg" alt="LinkedIn Icon" style={{ width: '30px', height: '50px' }} />
         </a>
+
         <a href="https://github.com/VirginiaWanjiru" target="_blank" rel="noopener noreferrer" title="GitHub">
           <img src="./images/github.svg" alt="GitHub Icon" style={{ width: '30px', height: '50px' }} />
         </a>
+        
         <div>
           <img src="./images/gmail.svg" alt="Gmail Icon" style={{ width: '30px', height: '50px' }} />
           <h3>virginiachristine55@gmail.com</h3>
--- reactapp/src/components/Hobbies.js
+++ reactapp/src/components/Hobbies.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 function Hobbies() {
   return (
+    <div className="Hobbies">
     <section id="Hobbies">
       <h2>Hobbies </h2>
       <ul>
-      <li>Meditation</li>
-        <li>Gardening</li>
-        <li>Reading</li>
+      <li>Meditation💭</li>
+        <li>Gardening🌱</li>
+        <li>Swimming🏊</li>
+        <li>Baking🧁</li>
       </ul>
     </section>
+    </div>
   );
 }
 
